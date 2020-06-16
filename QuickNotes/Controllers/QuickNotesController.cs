f0b5709271116a85d4c904713210f944981a1cd5
Ian Horner <ianmhorner@gmail.com>
2020-06-16 16:20:43 -0600

    Added Homepage and Notepad views

---
+++ QuickNotes/Controllers/QuickNotesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickNotes.Controllers
+{
+    public class QuickNotesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("save")]
+        public IActionResult Save()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuickNotes/Models/Note.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace QuickNotes.Models
+{
+    public class Note
+    {
+        public long Id { get; set; }
+        private string _key;
+        public string Key
+        {
+            get
+            {
+                if (_key == null)
+                {
+                    _key = Regex.Replace(Title.ToLower(), "[^a-z0-9]", "-");
+                }
+                return _key;
+            }
+            set { _key = value; }
+        }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Please enter a title.")]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Please enter body text.")]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        public DateTime Posted { get; set; }
+    }
+}
