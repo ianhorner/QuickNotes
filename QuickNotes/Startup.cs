4bdd7e43f273f9f17407f4c4bf0a8ab7c5881030
Ian Horner <ianmhorner@gmail.com>
2020-07-25 10:55:07 -0600

    Add developer exception page

--- QuickNotes/Startup.cs
+++ QuickNotes/Startup.cs
@@ -43,9 +43,10 @@ namespace QuickNotes
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
